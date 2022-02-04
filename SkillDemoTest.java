2ab61cabaf333854b47452058aacdacd05a8c4ba
fighterkabir <kshergill336@gmail.com>
2022-02-03 22:46:19 -0800

    lib + fail

---
+++ SkillDemoTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+
+public class SkillDemoTest {
+    
+    @Test
+    public void multiply() {
+        assertEquals(5, SkillDemo.multiply(2, 3)); // fail first
+    }
+
+    
+}
